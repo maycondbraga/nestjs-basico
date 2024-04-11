c9944e4060f1d41d09f99c6539f02036ab9dbc71
maycondbraga <maycondbraga.s@gmail.com>
2024-04-11 03:44:57 -0300

    feat: database postgres with docker and config

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn('increment')
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   age: number;
 }
