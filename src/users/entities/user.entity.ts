10dd7c116e03fc882e816d1907b59c1accd09a09
maycondbraga <maycondbraga.s@gmail.com>
2024-04-11 23:18:43 -0300

    feat: crud user service

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn('increment')
+  @PrimaryGeneratedColumn('identity')
   id: number;
 
   @Column()
